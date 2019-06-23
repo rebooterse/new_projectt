8ed3d98a45ae1fdfc0d124c67f111c44c5bfce97
Aren <aren.anushik@gmail.com>
2019-06-23 13:30:26 +0400

    ssskkk

---
+++ app/src/main/java/models/Shoes.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Shoes {
+    int image;
+
+    public Shoes(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
